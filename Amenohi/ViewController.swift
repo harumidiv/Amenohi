f41421fe352fc837589bc904da56727221d8d37a
佐川晴海 <sagawaharumi@sagawaharuminoMacBook-puro.local>
2019-07-14 10:55:30 +0900

    feat: Create a project

---
+++ Amenohi/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Amenohi
+//
+//  Created by 佐川晴海 on 2019/07/14.
+//  Copyright © 2019 佐川晴海. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
