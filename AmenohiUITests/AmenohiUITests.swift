79591a1cd22dd55a51655bc85c6ef39a3199d290
佐川晴海 <sagawaharumi@sagawaharuminoMacBook-puro.local>
2019-07-14 12:11:31 +0900

    feat: Add run script phase program

--- AmenohiUITests/AmenohiUITests.swift
+++ AmenohiUITests/AmenohiUITests.swift
@@ -9,7 +9,6 @@
 import XCTest
 
 class AmenohiUITests: XCTestCase {
-
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
 
@@ -30,5 +29,4 @@ class AmenohiUITests: XCTestCase {
         // Use recording to get started writing UI tests.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
-
 }
